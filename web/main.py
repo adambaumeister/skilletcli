b6230079231d1d702b852593933c41cd70cb1e50
Adam Baumeister <abigserve@gmail.com>
2019-08-04 17:45:26 +1000

    Add basic queries

--- web/main.py
+++ web/main.py
@@ -24,6 +24,10 @@ sys.path.append('web')
 import flask
 from flask import request
 from db import Firestore
+from jinja2 import Template, Environment, BaseLoader, meta
+from passlib.hash import md5_crypt
+
+
 
 app = flask.Flask(__name__)
 
@@ -37,8 +41,8 @@ def GetCollections():
     collections = firestore.GetCollections()
     return flask.jsonify(collections)
 
-@app.route('/skillet', methods=['GET'])
-def GetSnippets():
+@app.route('/skillets', methods=['GET'])
+def GetAllSnippets():
     firestore = Firestore()
     filters = {}
     for k, v in request.args.items():
@@ -51,3 +55,62 @@ def GetSnippets():
     for doc_ref in docs:
         l.append(doc_ref.to_dict())
     return flask.jsonify(l)
+
+@app.route('/snippet', methods=['POST'])
+def GetSnippet():
+    data = request.json
+
+    filters = data['filters']
+    skillet = data['skillet']
+
+    firestore = Firestore()
+    docs = firestore.GetDocumentSnaps(skillet, filters)
+    print("Query returned {} docs".format(len(docs)))
+    l = []
+    for doc_ref in docs:
+        doc_dict = doc_ref.to_dict()
+        if 'template_variables' in data:
+            pt = template(doc_dict["path"], data['template_variables'])
+            xt = template(doc_dict["xml"], data['template_variables'])
+            if not pt:
+                return error_message("Missing required template variables.")
+            if not xt:
+                return error_message("Missing required template variables.")
+            doc_dict["path"] = pt
+            doc_dict["xml"] = xt
+
+
+        l.append(doc_dict)
+    return flask.jsonify(l)
+
+
+def error_message(msg):
+    j = {
+        "result": "error",
+        "msg": msg
+    }
+    return flask.jsonify(j)
+
+def template(str, context):
+    e = Environment(loader=BaseLoader)
+    e.filters["md5_ hash"] = md5_hash
+
+    ast = e.parse(str)
+    vars = meta.find_undeclared_variables(ast)
+    for v in vars:
+        if v not in context:
+            print("Missing var: {}".format(v))
+            return
+    t = e.from_string(str)
+
+    return(t.render(context))
+
+# define functions for custom jinja filters
+def md5_hash(txt):
+    '''
+    Returns the MD5 Hashed secret for use as a password hash in the PanOS configuration
+    :param txt: text to be hashed
+    :return: password hash of the string with salt and configuration information. Suitable to place in the phash field
+    in the configurations
+    '''
+    return md5_crypt.hash(txt)
--- web/test_api.py
+++ web/test_api.py
@@ -1,17 +1,27 @@
-from .main import GetCollections
-from firestore import Firestore
+import requests
+"""
+To use this test suite in dev, first start Flask.
+"""
+TEST_URL="http://127.0.0.1:5000"
+TEST_SNIPPET_QUERY = {
+    "skillet": "iron-skillet",
+    "filters": {
+        "name": "shared_address",
+        "type": "panorama",
+        "stack": "snippets",
+    },
+    "template_variables": {
+        "SINKHOLE_IPV4": "1.1.1.1",
+    }
+}
+def test_get_snippets():
+    res = requests.post(TEST_URL+"/snippet", json=TEST_SNIPPET_QUERY)
+    print(res.json())
 
-def test_GetMeta():
-    firestore = Firestore()
-    m = firestore.get_meta()
-    print(m.Get_Collections())
-
-def test_AddCollection():
-    firestore = Firestore()
-    m = firestore.get_meta()
-    m.Add_Collection("TestCollection")
-
+def test_get_collections():
+    res = requests.get(TEST_URL)
+    print(res.json())
 
 if __name__ == "__main__":
-    test_GetMeta()
-    #test_AddCollection()
+    test_get_snippets()
+    #test_get_collections()
