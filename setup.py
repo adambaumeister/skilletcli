df8b1e49a0641556bff8df355b1e0f192167b9bb
abaumeister <abaumeister@paloaltonetworks.com>
2020-07-30 16:15:30 +1000

    Update version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name='skilletcli',
-    version='2.0.2',
+    version='3.0.0',
     packages=['Remotes', 'panosxml'],
     scripts=['skilletcli.py'],
     url='https://github.com/adambaumeister/skilletcli',
